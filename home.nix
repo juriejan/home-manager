f18cada0e786ae33e2dd3a69bebf50df11cfa191
Jurie-Jan Botha <juriejanbotha@gmail.com>
2025-12-27 11:23:57 +0200

    feat: added git alias

--- home.nix
+++ home.nix
@@ -81,6 +81,15 @@
     git = {
       enable = true;
       settings = {
+        alias = {
+          ad = "add";
+          br = "branch";
+          ci = "commit";
+          co = "checkout";
+          pl = "pull";
+          ps = "push";
+          st = "status";
+        };
         user = {
           name = "Jurie-Jan Botha";
           email = "juriejanbotha@gmail.com";
